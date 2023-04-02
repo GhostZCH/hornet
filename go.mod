274deccc8db3cdaec890a9df4b8db06276efdf06
zhaoch <zhao_chenhui_scu@163.com>
2023-04-02 18:04:05 +0800

    new

--- go.mod
+++ go.mod
@@ -1,10 +1,16 @@
 module hornet
 
-go 1.13
+go 1.18
+
+require github.com/valyala/fasthttp v1.45.0 // direct
 
 require (
-	github.com/ghostzch/asynclogger v0.0.0-20200204084903-6dcc3653f9f0
-	go.uber.org/zap v1.13.0
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0
-	gopkg.in/yaml.v2 v2.2.8
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/rs/zerolog v1.29.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
--- hornet.go
+++ hornet.go
@@ -1,53 +1,76 @@
 package main
 
 import (
-	"errors"
-	"flag"
-	"go.uber.org/zap"
-	"path/filepath"
-	"runtime/debug"
+	"fmt"
+	"hornet/common"
+	"math/rand"
+	"os"
+	"strings"
+	"syscall"
 	"time"
+
+	"github.com/valyala/fasthttp"
 )
 
-const VERSION int64 = 1000000
+func mmap(path string, size int) (data []byte, err error) {
+	var f *os.File
+	f, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0600)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
 
-func parseArgs() (string, string) {
-	path := flag.String("conf", "hornet.yaml", "conf file path")
-	mode := flag.String("mode", "cache", "start mode cache or proxy")
+	err = f.Truncate(int64(size))
+	if err != nil {
+		return nil, err
+	}
 
-	flag.Parse()
-	return *path, *mode
+	flag := syscall.PROT_READ | syscall.PROT_WRITE
+	return syscall.Mmap(int(f.Fd()), 0, size, flag, syscall.MAP_SHARED)
 }
 
 func main() {
-	path, mode := parseArgs()
-	dir, name := filepath.Split(path)
-	LoadConf(path, dir+"local_"+name)
-	InitLog()
-	Log.Warn("Conf", zap.Any("conf", Conf))
+	rand.Seed(time.Now().UnixNano())
 
-	defer func() {
-		if err := recover().(error); err != nil {
-			Log.Warn(string(debug.Stack()))
-			Log.Error("main", zap.NamedError("err", err))
-			time.Sleep(time.Duration(1) * time.Second)
-		}
-	}()
-
-	var h Handler
-
-	switch mode {
-	case "cache":
-		h = NewCacheHandler()
-	case "proxy":
-		h = NewProxyHandler()
-	default:
-		panic(errors.New("unknown mode:" + mode))
+	// store.NewSQLLite("test/meta")
+	path := "/dev/shm/random_file"
+	info, err := os.Stat(path)
+	if err != nil {
+		panic(err)
 	}
 
-	svr := NewServer(h)
+	data, err := mmap(path, int(info.Size()))
+	if err != nil {
+		panic(err)
+	}
 
-	go HandleSignal(svr)
+	logger := common.NewHourlyLogger("test/log/")
 
-	svr.Start()
+	// 创建 fasthttp 请求处理函数
+	handler := func(ctx *fasthttp.RequestCtx) {
+		switch string(ctx.Path()) {
+		case "/":
+			fmt.Fprintf(ctx, "Welcome to my website!")
+		case "/ping":
+			fmt.Fprintf(ctx, strings.Repeat("x", 4096))
+		case "/file":
+			ctx.SetContentType("text/plain")
+			ctx.Response.Header.Set("Content-Length", "4096")
+			off := 2000
+			if rand.Intn(10) < 100 {
+				// 90% in mem
+				off = rand.Intn(int(info.Size() - 4096))
+			}
+			ctx.Write(data[off : off+4096])
+		default:
+			ctx.Error("Unsupported path", fasthttp.StatusBadRequest)
+		}
+		logger.WriteLog(&common.LogData{Url: ctx.Path()})
+
+	}
+
+	// 启动 fasthttp 服务器
+	if err := fasthttp.ListenAndServe(":8080", handler); err != nil {
+		fmt.Printf("Error when starting server: %s\n", err.Error())
+	}
 }
